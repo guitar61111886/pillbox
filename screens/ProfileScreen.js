56e7fe1f1e0211563617a379517f8f5db4698c9b
guitar61111886 <guitarthunrada@gmail.com>
2021-05-23 20:18:10 +0700

    update profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,177 +1,131 @@
-// import React from 'react';
-// import {
-//     SafeAreaView, StyleSheet, ScrollView, View, Button, StatusBar, } from 'react-native';
-// import {Avatar, Title, Caption, Text, TouchableRipple} from 'react-native-paper';
-// import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-
-// const ProfileScreen = () => {
-//     return (
-//       <SafeAreaView style={styles.container} >
-//         <View style={styles.userInfoSection}>
-//           <View style={{flexDirection: 'row', marginTop: 15}}>
-//             <Avatar.Image
-//                                 source={require('../../Pillbox/src/imagegiga/gg.png')}
-//                                 size={80}
-//             />
-//             <View style={{marginLeft: 7}}>
-//               <Title style={[styles.title, {
-//                 marginTop: 15, marginBottom: 5
-//               }]}>Thunrada Choopromkaew</Title>
-//               <Caption style={styles.caption}>@giga61111886</Caption>
-//             </View>
-//           </View>
-//         </View>
-
-//         <View style={styles.userInfoSection}>
-//               <View style={styles.row}>
-//                 <Icon name="map-marker-radius" color="#16A085" size={20} />
-//                 <Text style={{color: '#777777', marginLeft: 16}}>Nakhon Si Thammarat, Thailand</Text>
-//               </View>
-//               <View style={styles.row}>
-//                 <Icon name="phone" color="#16A085" size={20} />
-//                 <Text style={{color: '#777777', marginLeft: 16}}>094-569-9615</Text>
-//               </View>
-//               <View style={styles.row}>
-//                 <Icon name="email" color="#16A085" size={20} />
-//                 <Text style={{color: '#777777', marginLeft: 16}}>guitarthunrada@gmail.com</Text>
-//               </View>
-//         </View>
-
-//         <View style={styles.infoBoxWrapper}>
-//               <View style={[styles.infoBox, {
-//                 borderRightColor: '#dddddd',
-//                 borderRightWidth: 2
-//               }]}>
-//                 <Title>12</Title>
-//                 <Caption>Reminded</Caption>
-//               </View>
-
-//               <View style={styles.infoBox}>
-//                 <Title>30</Title>
-//                 <Caption>Remind/Month</Caption>
-//               </View>
-//         </View>
-
-//         <View style={styles.menuWrapper}>
-//               <TouchableRipple onPress={() => {}}>
-//                 <View style={styles.menuItem}>
-//                   <Icon name="heart-outline" color="#FF6347" size={20}/>
-//                   <Text style={styles.menuItemText}>Your Favorites</Text>
-//                 </View>
-//               </TouchableRipple>
-
-//               <TouchableRipple onPress={() => {}}>
-//                 <View style={styles.menuItem}>
-//                   <Icon name="share-outline" color="#FF6347" size={20}/>
-//                   <Text style={styles.menuItemText}>Tell Your Friend</Text>
-//                 </View>
-//               </TouchableRipple>
-
-//               <TouchableRipple onPress={() => {}}>
-//                 <View style={styles.menuItem}>
-//                   <Icon name="account-check-outline" color="#FF6347" size={20}/>
-//                   <Text style={styles.menuItemText}>Support</Text>
-//                 </View>
-//               </TouchableRipple>
-
-//               <TouchableRipple onPress={() => {}}>
-//                 <View style={styles.menuItem}>
-//                   <Icon name="cog-outline" color="#FF6347" size={20}/>
-//                   <Text style={styles.menuItemText}>Settings</Text>
-//                 </View>
-//               </TouchableRipple>
-//         </View>
-//       </SafeAreaView>
-//     );
-//   };
-
-// export default ProfileScreen;  
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//     },
-//     userInfoSection: {
-//       paddingHorizontal: 30,
-//       marginBottom: 25,
-//     },
-//     title: {
-//       fontSize: 24,
-//       fontWeight: 'bold',
-//     },
-//     caption: {
-//       fontSize: 14,
-//       lineHeight: 14,
-//       fontWeight: '500',
-//     },
-//     row: {
-//       flexDirection: 'row',
-//       marginBottom: 10,
-//     },
-//     infoBoxWrapper: {
-//       borderBottomColor: '#dddddd',
-//       borderBottomWidth: 1,
-//       borderTopColor: '#dddddd',
-//       borderTopWidth: 1,
-//       flexDirection: 'row',
-//       height: 100,
-//     },
-//     infoBox: {
-//       width: '50%',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//     },
-//     menuWrapper: {
-//       marginTop: 10,
-//     },
-//     menuItem: {
-//       flexDirection: 'row',
-//       paddingVertical: 15,
-//       paddingHorizontal: 30,
-//     },
-//     menuItemText: {
-//       color: '#777777',
-//       marginLeft: 20,
-//       fontWeight: '600',
-//       fontSize: 16,
-//       lineHeight: 26,
-//     },
-// });
-
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import type {Node} from 'react';
-
+// import type { Node } from 'react';
 import {
+  FlatList,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
   useColorScheme,
   View,
-  Button,
+  ActivityIndicator,
+  Text,
+  Image, Avatar
 } from 'react-native';
+import { Container, Header, Content, List, ListItem, Left, Body, Right, Thumbnail, Item, Input, Icon, Button } from 'native-base';
 
-const NotificationScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.paddingProfile}>
-      <Text style={styles.SettimeText}> Profile Screen</Text>
+export default class NotificationScreen extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      fullData: [],
+      loading: false,
+      error: null,
+      query: ""
+    }
+  }
+
+  componentDidMount() {
+    this.requestAPI()
+  }
+
+  requestAPI = () => {
+    this.setState({ loading: true })
+    const apiURL = "http://172.20.10.5/API/profileapi.php"
+    fetch(apiURL).then((res) => res.json())
+      .then((json) => {
+        console.log('JSON', json)
+        this.setState({
+          loading: false,
+          data: json,
+          // fullData: json,
+          filterPill: json
+        })
+      }).catch(error => {
+        console.error(error);
+        this.setState({ data: [] })
+        this.setState({ error, loading: false })
+      })
+  }
+
+  _renderItem = ({ item, index }) => {
+    return (
+
+      <ListItem style={{
+        backgroundColor: '#FFD180',
+        marginHorizontal: 15,
+        marginVertical: 5,
+        borderRadius: 10,
+        borderColor: "#6D4C41",
+        borderWidth: 1,
+        padding: 10
+      }}>
+        <Body>
+          <Text style={styles.sectionTitle}>{item.firstname}  {item.lastname}
+          </Text>
+          <Text style={styles.sectionTitle}>{item.email}
+          </Text>
+        </Body>
+      </ListItem>
+    )
+  }
+
+  renderFooter = () => {
+    if (!this.state.loading) return null
+    return (
+      <View style={{ paddingVertical: 20, borderTopWidth: 1, borderColor: "EE6A59" }}>
+        <ActivityIndicator animating size="large" />
       </View>
-      <Button
-        title="go to remind screen"
-        onPress={() => navigation.navigate("Notification")}
-      />
-    </View>
-  );
+    )
+  }
+
+  // onChangeText(text){
+  //   console.log('textChanged', text)
+  //   let filterArray = this.state.filterPill
+  //   let searchResult = filterArray.filter(data => 
+  //     data.meal_alert.includes(text)
+  //     )
+  //     this.setState({filterPill : searchResult})
+  // }
+
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {/* // <Container> */}
+        <View style={styles.paddingSettime}>
+        <Text style={styles.SettimeText}>ข้อมูลส่วนตัว</Text>
+      </View>
+        <View style={styles.profilepic}>
+          <Image source={require('../../project/src/profile.jpg')}
+            style={{ width: 150, height: 150, marginTop: 20}}
+          />
+        </View>
+
+      {/* <View style={{marginLeft: 15}}>
+        <Text style={styles.SetNameText1}>Name : </Text>
+      </View> */}
+
+        <View style={{marginTop: 15}}>
+          <FlatList
+            data={this.state.data}
+            renderItem={this._renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            ListFooterComponent={this.renderFooter}
+          />
+        </View>
+        {/* </Container> */}
+      </ScrollView>
+    )
+  }
 };
 
-export default NotificationScreen;
 
 const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
   container: {
     flex: 1,
     //flexDirection: 'column',
@@ -179,15 +133,22 @@ const styles = StyleSheet.create({
     //justifyContent: 'flex-start',
     //alignItems: 'center',
   },
-
   SettimeText: {
     color: 'white',
-    fontWeight: 'bold',
+    fontWeight: '300',
     fontSize: 30,
     fontFamily: "Cochin",
+    textAlign: 'center',
   },
-
-  paddingProfile: {
+  SetNameText1: {
+    color: 'white',
+    fontWeight: '300',
+    fontSize: 25,
+    fontFamily: "Cochin",
+    marginLeft: 25
+    // textAlign: 'justify'
+  },
+  paddingSettime: {
     padding: 10,
     backgroundColor: '#EE6A59',
     marginTop: 20,
@@ -197,4 +158,84 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 20,
     marginHorizontal: 90,
   },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  profilepic: {
+    // justifyContent: 'center',
+    // alignContent: 'center',
+    // display: "flex",
+    alignItems: "center",
+    // flexDirection: 'column'
+  }
 });
+
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// // import { createDrawerNavigator } from '@react-navigation/drawer';
+// // import type {Node} from 'react';
+
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   View,
+//   Button,
+// } from 'react-native';
+
+// const NotificationScreen = ({ navigation }) => {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.paddingProfile}>
+//       <Text style={styles.SettimeText}> Profile Screen</Text>
+//       </View>
+//       {/* <Button
+//         title="go to remind screen"
+//         onPress={() => navigation.navigate("Notification")}
+//       /> */}
+//     </View>
+//   );
+// };
+
+// export default NotificationScreen;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     //flexDirection: 'column',
+//     backgroundColor: '#50A3A4',
+//     //justifyContent: 'flex-start',
+//     //alignItems: 'center',
+//   },
+
+//   SettimeText: {
+//     color: 'white',
+//     fontWeight: 'bold',
+//     fontSize: 30,
+//     fontFamily: "Cochin",
+//   },
+
+//   paddingProfile: {
+//     padding: 10,
+//     backgroundColor: '#EE6A59',
+//     marginTop: 20,
+//     borderTopLeftRadius: 20,
+//     borderTopRightRadius: 20,
+//     borderBottomLeftRadius: 20,
+//     borderBottomRightRadius: 20,
+//     marginHorizontal: 90,
+//   },
+// });
